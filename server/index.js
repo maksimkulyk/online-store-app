1dfe7df3b146dd437419e0f84df3b03bc54d8bad
Maksim Kulyk <kulykmaksim@gmail.com>
2021-08-09 21:24:21 +0300

    feat: connect to database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 require("dotenv").config();
+const sequelize = require("./db");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 4000;
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.listen(PORT, () =>
-  console.log(`Server is running on http://localhost:${PORT}`)
-);
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () =>
+      console.log(`Server is running on http://localhost:${PORT}`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
