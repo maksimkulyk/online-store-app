59f85f4f0ba54c83760d394d65cdd430b96b2834
Maksim Kulyk <kulykmaksim@gmail.com>
2021-08-10 19:56:20 +0300

    feat: add error handling, add CRUD for types, brands and devices

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,7 +1,7 @@
 class UserController {
   async registration(req, res) {}
   async login(req, res) {}
-  async check(req, res) {}
+  async check(req, res, next) {}
 }
 
 module.exports = new UserController();
--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
 const sequelize = require("./db");
 const models = require("./models/models");
 const cors = require("cors");
+const fileUpload = require("express-fileupload");
 const router = require("./routes/index");
+const errorHandler = require("./middleware/ErrorHandling.middleware");
+const path = require("path");
 
 const PORT = process.env.PORT || 4000;
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "static")));
+app.use(fileUpload({}));
 app.use("/api", router);
+
+app.use(errorHandler);
 
 const start = async () => {
   try {
